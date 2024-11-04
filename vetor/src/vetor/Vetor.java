aa7ed0e6a506a08bb0b88629d43564af87be99f9
snsup <snsup@marcosjunior>
2024-11-04 11:32:13 -0300

    terceiro commit

--- vetor/src/vetor/Vetor.java
+++ vetor/src/vetor/Vetor.java
@@ -3,7 +3,6 @@ public class Vetor{
 	public static void main(String[]args) {
 		String [] nomes = new String[1];
 		nomes[0]="Marcos";
-		nomes[1]="joao";nomes[1]="joao";
 		System.out.println(nomes[0]);
 		
 	}
